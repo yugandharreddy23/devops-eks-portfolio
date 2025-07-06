23b584b02100d28eb5d20f2b644edaad34dc7a2f
Badri <badri@Mac.fritz.box>
2025-07-06 15:30:38 +0200

    Updated provider.tf file

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -12,7 +12,6 @@ provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.this.token
-  depends_on = [module.eks]
 }
 
 
