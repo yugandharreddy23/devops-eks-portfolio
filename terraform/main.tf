ca4c34b1d6eb5b00816856b52f0902dab7aa167e
Badri <badri@Mac.fritz.box>
2025-07-06 15:32:28 +0200

    Updated main.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,15 @@
 
 data "aws_caller_identity" "current" {}
 
+data "aws_eks_cluster" "this" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+}
+
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.8.5"
