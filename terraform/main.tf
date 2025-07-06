dcaa472fd03aa5e189be895ab45026feb2cd4fe8
Badri <badri@Mac.fritz.box>
2025-07-06 15:16:03 +0200

    Updated auth config file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,6 @@
-# Get current AWS caller identity
+# main.tf
+
 data "aws_caller_identity" "current" {}
-
-data "aws_eks_cluster_auth" "this" {
-  name = module.eks.cluster_name
-}
-
-# Configure the Kubernetes provider to connect to the newly created EKS cluster
-# This configuration relies on outputs from the EKS module and the aws_eks_cluster_auth data source
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.this.token
-
-  # Set a short timeout if you're frequently hitting issues, but default is usually fine
-  # exec {
-  #   api_version = "client.authentication.k8s.io/v1beta1"
-  #   command     = "aws"
-  #   args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", local.region]
-  # }
-}
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
@@ -28,22 +10,15 @@ module "eks" {
   cluster_endpoint_public_access = true
 
   cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
+    coredns = { most_recent = true }
+    kube-proxy = { most_recent = true }
+    vpc-cni = { most_recent = true }
   }
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
 
-  # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
     ami_type       = "BOTTLEROCKET_x86_64"
     instance_types = ["m5.large"]
@@ -54,45 +29,33 @@ module "eks" {
       min_size       = 1
       max_size       = 2
       desired_size   = 1
-
       instance_types = ["t3.medium"]
       capacity_type  = "ON_DEMAND"
-
       tags = {
         ExtraTag = "helloworld"
       }
     }
   }
 
-  tags = local.tags
-}
-# **NEW:** Manage aws-auth ConfigMap directly with Kubernetes provider
-# This replaces the entire null_resource.aws_auth block
-resource "kubernetes_config_map" "aws_auth" {
-  metadata {
-    name      = "aws-auth"
-    namespace = "kube-system"
-  }
-  data = {
-    mapRoles = <<-EOT
-      - rolearn: ${var.github_actions_role_arn}
-        username: github-actions
-        groups:
-          - system:masters
-    EOT
-    mapUsers = <<-EOT
-      - userarn: ${data.aws_caller_identity.current.arn}
-        username: admin
-        groups:
-          - system:masters
-    EOT
-  }
+  manage_aws_auth = true
 
-  # Ensure this resource is created only after the EKS cluster is fully
-  # provisioned and the Kubernetes provider can successfully authenticate.
-  depends_on = [
-    module.eks # Ensures EKS cluster is ready
+  aws_auth_roles = [
+    {
+      rolearn  = var.github_actions_role_arn
+      username = "github-actions"
+      groups   = ["system:masters"]
+    }
   ]
+
+  aws_auth_users = [
+    {
+      userarn  = data.aws_caller_identity.current.arn
+      username = "admin"
+      groups   = ["system:masters"]
+    }
+  ]
+
+  tags = local.tags
 }
 
 module "vpc" {
