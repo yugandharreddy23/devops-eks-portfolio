f0bfae54b835757a5b5cfc24baf3038ed0d31c62
Badri <badri@Mac.fritz.box>
2025-07-06 13:34:17 +0200

    Updated variables.tf file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,5 @@
-# Variable for GitHub Actions role ARN
-variable "github_actions_role_arn" {
-  description = "ARN of the GitHub Actions IAM role"
+variable "github_role_arn" {
+  description = "GitHub OIDC role ARN"
   type        = string
   default     = ""
 }
